6a668b73ea10660cd3e760a95784c77e5ca0ee91
Cedrik <cedrik.git@tryninja.io>
2021-09-14 17:59:06 -0500

    fix: status

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,6 +1,4 @@
-import Arweave from 'arweave';
 import clc from 'cli-color';
-import { debug } from 'console';
 import ArgumentsInterface from '../faces/arguments';
 import CommandInterface from '../faces/command';
 import { status } from '../lib/status';
@@ -8,55 +6,55 @@ import { getArweaveUri } from '../utils/utils';
 
 const command: CommandInterface = {
   name: 'status',
-  aliases: ['stat'],
+  aliases: ['s'],
   description: 'Check the status of a transaction ID',
   useOptions: true,
   args: ['txid'],
   execute: async (args: ArgumentsInterface): Promise<void> => {
-    const { commandValue: txid, arweave } = args;
+    const { commandValue: txid, arweave, debug } = args;
 
     const arweaveUri = getArweaveUri(arweave);
+    try {
+      const res = await status(txid, arweave);
 
-    status(txid, arweave)
-      .then((res) => {
-        let responseStatus = '';
-        switch (res.status) {
-          case 200:
-            responseStatus = clc.green('200 - Accepted');
-            break;
-          case 202:
-            responseStatus = clc.yellow('202 - Pending');
-            break;
-          case 400:
-            responseStatus = clc.red(`400 - ${res.errorMessage}`);
-            break;
-          case 404:
-            responseStatus = clc.red(`404 - Not Found`);
-            break;
-          default:
-            responseStatus = clc.red(`${res.status} - ${res.errorMessage}`);
-            break;
-        }
-        console.log(`Trasaction ID: ${clc.blue(txid)}
+      console.log("🚀 ~ file: status.ts ~ line 20 ~ .then ~ res", res)
+      let responseStatus = '';
+      switch (res.status) {
+        case 200:
+          responseStatus = clc.green('200 - Accepted');
+          break;
+        case 202:
+          responseStatus = clc.yellow('202 - Pending');
+          break;
+        case 400:
+          responseStatus = clc.red(`400 - ${res.errorMessage}`);
+          break;
+        case 404:
+          responseStatus = clc.red(`404 - Not Found`);
+          break;
+        default:
+          responseStatus = clc.red(`${res.status} - ${res.errorMessage}`);
+          break;
+      }
+      console.log(`Trasaction ID: ${clc.blue(txid)}
 
 Status: ${responseStatus}`);
 
-        if (res.status === 200) {
-          console.log(` - Block: ${res.blockHeight}
- - Block hash: ${res.blockHash}
- - Confirmations: ${res.confirmations}
+      if (res.status === 200) {
+        console.log(` - Block: ${clc.cyan(res.blockHeight)}
+ - Block hash: ${clc.cyan(res.blockHash)}
+ - Confirmations: ${clc.cyan(res.confirmations)}
 
-Transaction URL: ${arweaveUri}/${txid}
-Block URL: ${arweaveUri}/block/hash/${res.blockHash}
+Transaction URL: ${clc.cyan(`${arweaveUri}/${txid}`)}
+Block URL: ${clc.cyan(`${arweaveUri}/block/hash/${res.blockHash}`)}
 
-Transaction explorer URL: https://viewblock.io/arweave/tx/${txid}
-Block explorer URL: https://viewblock.io/arweave/block/${res.blockHeight}`);
-        }
-      })
-      .catch((e) => {
-        console.log(clc.red(`Unable to reach ${getArweaveUri(arweave)} - ${e.message}`));
-        if (debug) console.log(e);
-      });
+Transaction explorer URL: ${clc.cyan(`https://viewblock.io/arweave/tx/${txid}`)}
+Block explorer URL: ${clc.cyan(`https://viewblock.io/arweave/block/${res.blockHeight}`)}`);
+      }
+    } catch (e) {
+      console.log(clc.red(`Unable to reach ${getArweaveUri(arweave)} - ${e.message}`));
+      if (debug) console.log(e);
+    }
   }
 };
 
--- src/lib/status.ts
+++ src/lib/status.ts
@@ -4,22 +4,16 @@ export async function status(
   txid: string,
   arweave: Arweave,
 ): Promise<{ status: number; blockHeight: number; blockHash: string; confirmations: number; errorMessage?: string }> {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const res = await arweave.api.get(txid);
-      if (res.status !== 200 && res.status !== 202) {
-        resolve({ status: res.status, blockHeight: -1, blockHash: '', confirmations: -1, errorMessage: res.data });
-      }
+  const res = await arweave.api.get(txid);
+  if (res.status !== 200 && res.status !== 202) {
+    return { status: res.status, blockHeight: -1, blockHash: '', confirmations: -1, errorMessage: res.data };
+  }
 
-      const { data } = await arweave.api.get(`tx/${txid}/status`);
-      resolve({
-        blockHeight: data.block_height,
-        blockHash: data.block_indep_hash,
-        confirmations: data.number_of_confirmations,
-        status: res.status,
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
+  const { data } = await arweave.api.get(`tx/${txid}/status`);
+  return {
+    blockHeight: data.block_height,
+    blockHash: data.block_indep_hash,
+    confirmations: data.number_of_confirmations,
+    status: res.status,
+  };
 }
