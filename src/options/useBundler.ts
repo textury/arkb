f1becae55644466a7461d3a5d89c9656d1c90845
Cedrik <cedrik.git@tryninja.io>
2021-10-15 08:13:23 -0500

    fix: bundler new uri

--- src/options/useBundler.ts
+++ src/options/useBundler.ts
@@ -4,7 +4,7 @@ const option: OptionInterface = {
   name: 'use-bundler',
   description: 'Use an ans104 bundler',
   arg: 'host_or_ip',
-  usage: 'http://bundler.arweave.net:10000',
+  usage: 'http://bundler.arweave.net',
 };
 
 export default option;
