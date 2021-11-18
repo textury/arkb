c18b38940111fea212fef76a14aa9b67a3723c1b
fuadop <fuadolatunji@gmail.com>
2021-11-18 02:40:26 +0100

    fix: return only tx id

--- src/commands/fundBundler.ts
+++ src/commands/fundBundler.ts
@@ -60,7 +60,6 @@ const command: CommandInterface = {
       await blockweave.transactions.sign(tx, wallet);
       await blockweave.transactions.post(tx);
 
-      const url = `https://arweave.net/${tx.id}`;
 
       console.log(
         `${clc.cyan(addy)} bundler has been funded with ${clc.yellow(
@@ -68,7 +67,7 @@ const command: CommandInterface = {
         )}`,
       );
 
-      console.log(clc.green(url));
+      console.log(clc.green(tx.id));
     } catch (e) {
       clc.red('Error funding bundler address, see more info with the --debug option.');
       if (debug) console.log(e);
