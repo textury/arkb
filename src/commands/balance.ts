4ecffd70c848a3b68b36303f6e50cfe39f19b013
fuadop <fuadolatunji@gmail.com>
2022-01-10 22:25:41 +0100

    chore: add --no-colors support to balance command

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -1,4 +1,3 @@
-import clc from 'cli-color';
 import ArgumentsInterface from '../faces/arguments';
 import CommandInterface from '../faces/command';
 import { getWallet } from '../utils/wallet';
@@ -8,21 +7,23 @@ import walletOption from '../options/wallet';
 import debugOption from '../options/debug';
 import helpOption from '../options/help';
 import useBundlerOption from '../options/useBundler';
+import noColorsOption from '../options/noColors';
 import { JWKInterface } from 'blockweave/dist/faces/lib/wallet';
 import Bundler from '../utils/bundler';
+import { parseColor } from '../utils/utils';
 
 const command: CommandInterface = {
   name: 'balance',
   aliases: ['b'],
   description: 'Get the current balance of your wallet',
-  options: [gatewayOption, timeoutOption, walletOption, debugOption, helpOption, useBundlerOption],
+  options: [gatewayOption, timeoutOption, walletOption, debugOption, helpOption, useBundlerOption, noColorsOption],
   execute: async (args: ArgumentsInterface): Promise<void> => {
-    const { walletPath, config, debug, blockweave, useBundler, bundler } = args;
+    const { walletPath, config, debug, blockweave, useBundler, bundler, colors } = args;
 
     const wallet: JWKInterface = await getWallet(walletPath, config, debug);
 
     if (!wallet) {
-      console.log(clc.red('Please set a wallet or run with the --wallet option.'));
+      console.log(parseColor(colors, 'Please set a wallet or run with the --wallet option.', 'red'));
       return;
     }
 
@@ -30,7 +31,7 @@ const command: CommandInterface = {
     try {
       addy = await blockweave.wallets.jwkToAddress(wallet);
     } catch (e) {
-      console.log(clc.red('Unable to decrypt wallet address.'));
+      console.log(parseColor(colors, 'Unable to decrypt wallet address.', 'red'));
       if (debug) console.log(e);
     }
 
@@ -39,12 +40,14 @@ const command: CommandInterface = {
         const bal: number = await Bundler.getAddressBalance(bundler, addy);
 
         console.log(
-          `${clc.cyan(addy)} has a bundler balance of ${clc.yellow(
+          `${parseColor(colors, addy, 'cyan')} has a bundler balance of ${parseColor(
+            colors,
             `AR ${blockweave.ar.winstonToAr(bal.toString(), { formatted: true, decimals: 12, trim: true })}`,
+            'yellow',
           )}`,
         );
       } catch (e) {
-        console.log(clc.red('Unable to retrieve bundler balance.'));
+        console.log(parseColor(colors, 'Unable to retrieve bundler balance.', 'red'));
         if (debug) console.log(e);
       }
       return;
@@ -53,12 +56,14 @@ const command: CommandInterface = {
     try {
       const bal = await blockweave.wallets.getBalance(addy);
       console.log(
-        `${clc.cyan(addy)} has a balance of ${clc.yellow(
+        `${parseColor(colors, addy, 'cyan')} has a balance of ${parseColor(
+          colors,
           `AR ${blockweave.ar.winstonToAr(bal, { formatted: true, decimals: 12, trim: true })}`,
+          'yellow',
         )}`,
       );
     } catch (e) {
-      console.log(clc.red('Unable to retrieve wallet balance.'));
+      console.log(parseColor(colors, 'Unable to retrieve wallet balance', 'red'));
       if (debug) console.log(e);
     }
   },
